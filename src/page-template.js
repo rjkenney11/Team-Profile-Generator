f690e45faa75adeccfde5c065bfae7057f7a106a
rjkenney11 <rachelkenney@Rachels-MBP.fios-router.home>
2021-03-19 21:14:39 -0400

    final changes

--- src/page-template.js
+++ src/page-template.js
@@ -109,7 +109,6 @@ const layout = team => {
     return `
     <!DOCTYPE html>
     <html lang="en">
-    <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Team Profile Generator</title>
@@ -117,6 +116,8 @@ const layout = team => {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
         integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Poiret+One&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Fredericka+the+Great&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/d3bce829f3.js" crossorigin="anonymous"></script>
     </head>
     <h1> Our Team: </h1>
